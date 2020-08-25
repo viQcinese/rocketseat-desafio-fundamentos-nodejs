3aef166a311cfb51c14f6af5f5dc5da99fddfa3b
Vitor Senise <vitor.senise@gmail.com>
2020-08-25 18:43:31 -0300

    Initial commit, passing all tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,35 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'income') {
+        return accumulator + currentValue.value;
+      }
+      return accumulator;
+    }, 0);
+
+    const outcome = this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'outcome') {
+        return accumulator + currentValue.value;
+      }
+      return accumulator;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
